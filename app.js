504ee1e36ee6908b295e0dbd9213039b7372e73d
Ankit Shaji <ankitshaji@gmail.com>
2022-08-05 19:21:35 +1200

    comments+

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-//main file
+//main file of an app that gets other npm package modules or user created modules
 //key/variables - Object destructuring
 const { PI, square } = module.require("./math"); //when its not existing module in node_modules
 //self created file then we need ./
 const cats = require("./shelter"); //folder/directory//looks for index.js in it
-console.dir(PI); 
-console.dir(cats); //array of objects was what was exported from index.js 
+console.dir(PI);
+console.dir(cats); //array of objects was what was exported from index.js
--- npm.js
+++ npm.js
@@ -1,3 +1,4 @@
+//Basics of NPM
 //NPM is a libarary of packages/tools other people have written
 //npm is a command line tool to install these packages in our node projects
 //auto installed with nodejs runtime
@@ -18,9 +19,9 @@
 //we also put it inside all node apps/modules/packages we make
 //it contains metadata/information about the current project/package/module
 //(npm i packageName) adds packageNames into "dependencies" in package.json
-//"dependencies": {"packageName":"versionNo",} 
+//"dependencies": {"packageName":"versionNo",}
 // when node_modules is ommited/deleted - node looks in package.json
-//to auto install dependencies of the current project/package/module  with command (npm install) 
+//to auto install dependencies of the current project/package/module  with command (npm install)
 
 //adding 3 npm packages - adding to node_modules and package.json's dependencies
 const jokes = require("give-me-a-joke"); //since its a package/module we use its name
@@ -29,7 +30,7 @@ const colors = require("colors"); //since its a package/module we use its name
 // console.dir(colors);//colors is an object with methods and properties in it
 // const cowsay = require("cowsay") //npm link cowsay  //we need to link if we want to use global package locally
 // console.dir(cowsay)
-const figlet = require("figlet") //get module/package
+const figlet = require("figlet"); //get module/package
 // console.dir(figlet) //functionObject
 //look at how to use each package on the npm website
 
@@ -39,11 +40,11 @@ jokes.getRandomDadJoke(function (joke) {
 });
 console.log(colors.green("hello")); // outputs green text //method takes in argument of string
 //function expression has callback we execute when ready
-figlet('Hello World!!', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(colors.green(data))
+figlet("Hello World!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(colors.green(data));
 });
